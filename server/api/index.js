e76500b007f1823138900dcc8339b3f0b19dd2bd
pixelbuildlab <2017n2801@gmail.com>
2024-08-31 15:30:04 +0500

    allowed all urls

--- server/api/index.js
+++ server/api/index.js
@@ -24,12 +24,7 @@ const app = express()
 app.use(
   cors({
     origin: function (origin, callback) {
-      console.log(origin)
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
+      callback(null, true)
     },
     credentials: true,
   })
